f3c50ce06a79a25f97a0660b52d931053afce6fc
Ferd <60294740+papaferd@users.noreply.github.com>
2020-01-27 19:21:37 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -6,12 +6,12 @@ header('Content-Type: text/html; charset=utf8');
 $data = array('tarif_id'=>'5');
 $data_json = json_encode($data);
 
+///Изменить имя сайта при тестировании
 $url ='http://test3.ru/users/services/3/tarif';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_AUTOREFERER,false);
-
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/json','Content-Length: '.strlen($data_json)));
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
 curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
