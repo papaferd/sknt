73ff9a7a11cb851fc8d2882361d0a3c89191a4ad
Ferd <60294740+papaferd@users.noreply.github.com>
2020-01-27 18:49:45 +0300

    Create 1.php
    Test script File

---
+++ 1.php
@@ -0,0 +1,22 @@
+<?php
+header('HTTP/1.0 200 OK');	
+header('Content-Type: text/html; charset=utf8');
+
+
+$data = array('tarif_id'=>'5');
+$data_json = json_encode($data);
+
+$url ='http://test3.ru/users/services/3/tarif';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_AUTOREFERER,false);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/json','Content-Length: '.strlen($data_json)));
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response  = curl_exec($ch);
+
+
+print_r($response);
